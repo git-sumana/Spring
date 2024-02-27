8c996e376c8e401c7bda41b160e5451a6febaac5
Sumana Datta Kapavarapu <137371757+git-sumana@users.noreply.github.com>
2024-02-27 13:11:42 +0530

    Add files via upload

---
+++ Laptop.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+    public void compile()
+    {
+        System.out.println("Compiling...");
+    }
+}
